68ba095f9a0f881e5bbbe9e6fcdbb3cf89b2e89f
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-23 23:35:41 +0700

    renewbook

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -37,4 +37,48 @@ def index(request):
                  'num_instances_available': num_instances_available, 'num_authors': num_authors,
                  'num_visits': num_visits},
     )
-=
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 1
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 1
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+    if request.method == 'POST':
+        form = RenewBookForm(request.POST)
+        if form.is_valid():
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+            return HttpResponseRedirect(reverse('all-borrowed'))
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst': book_inst})
